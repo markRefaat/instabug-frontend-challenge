308a4622bfad042a5f70867525dd35433231bf38
Mark Refaat <mark1711712@miuegypt.edu.eg>
2021-07-17 17:34:02 +0200

    Mobile Header
    Create Mobile Header with:
    - Youtube Icon
    - Search Toggle Icon
    - input field (show / hide when press search icon)

---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+    <header>
+        <img id="youtube_mobile_icon" alt="Youtube Icon" src="../assets/youtubeMobileIcon.png" />
+
+        <p v-if="!state">{{text}}</p>
+        <input v-if="state" type="text" placeholder="Search" name="search" v-model="text"/>
+        <img @click="toggleSearchInput" id="search_icon" alt="Search Icon" src="../assets/search.png" />
+
+    </header>
+</template>
+
+
+
+<script>
+export default {
+    name: "Header",
+    data() {
+        return {
+            state: false,
+            text: ""
+        }
+    },
+    methods: {
+        toggleSearchInput(){
+            this.state = !this.state
+        }
+    }
+}
+</script>
+
+<style scoped>
+header {
+    height: 60px;
+    background-color: red;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+#youtube_mobile_icon {
+    padding-left: 15px;
+    padding-right: 15px;
+    width: 50px;
+}
+input {
+    font-size: 23px;
+    margin-right:  auto;
+    width: 60%;
+}
+p {
+    font-size: 23px;
+    margin-right:  auto;
+    color: white;
+}
+#search_icon {
+    padding-right: 15px;
+    width: 25px;
+}
+</style>
