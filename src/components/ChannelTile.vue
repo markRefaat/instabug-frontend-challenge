6a9315d73da226ef671a1fe9b6b4239fddc635ea
Mark Refaat <mark1711712@miuegypt.edu.eg>
2021-07-17 17:35:24 +0200

    Channel Tile
    The channel template
    - Circular Image
    - Channel Name
    - Channel Subscribers
    - Channel Videos Count

---
+++ src/components/ChannelTile.vue
@@ -0,0 +1,50 @@
+<template>
+    <a href="#" class="channel-container">
+        <img class="channel-icon" src="http://unsplash.it/36/36?gravity=center" />
+        <div class="channel-details">
+            <div class="channel-title">SpongeBob SquarePants</div>
+            <div class="channel-data">149 Videos <br> 23,858 subscribers</div>
+        </div>    
+    </a>
+</template>
+
+<script>
+export default {
+  name: "ChannelTile"
+}
+</script>
+
+
+<style scoped>
+
+.channel-container {
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+  margin-left: 10px;
+  text-decoration: none;
+}
+
+.channel-icon {
+  margin-right: 15px;
+  border-radius: 50%;
+  background-color: #AAA;
+  width: 100px;
+  height: 100px;
+}
+
+.channel-details {
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+}
+
+.channel-title {
+  font-size: 20px;
+  color: black;
+}
+
+.channel-data {
+  color: #555;
+}
+</style>
