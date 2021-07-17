f833ebf032049a633e5cbd98aa9f854263ad765c
Mark Refaat <mark1711712@miuegypt.edu.eg>
2021-07-17 17:36:21 +0200

    Video Search Tile
    The video search template
    - Rectangular Image
    - Video Name
    - Video Channel
    - Video Views Count

---
+++ src/components/VideoTile.vue
@@ -0,0 +1,50 @@
+<template>
+    <a href="#" class="video-container">
+        <img class="video-icon" src="http://unsplash.it/36/36?gravity=center" />
+        <div class="video-details">
+            <div class="video-title">SpongeBob in Real Life Episode 2</div>
+            <div class="video-data">JChaseFilms <br> 54,013,577 views</div>
+        </div>    
+    </a>
+</template>
+
+<script>
+export default {
+  name: "VideoTile"
+}
+</script>
+
+
+
+<style scoped>
+
+.video-container {
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+  margin-left: 10px;
+  text-decoration: none;
+}
+
+.video-icon {
+  margin-right: 15px;
+  background-color: #AAA;
+  width: 300px;
+  height: 100px;
+}
+
+.video-details {
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+}
+
+.video-title {
+  font-size: 20px;
+  color: black;
+}
+
+.video-data {
+  color: #555;
+}
+</style>
