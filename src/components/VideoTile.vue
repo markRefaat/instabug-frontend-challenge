33acf5c285c43c00e46871e53c5c0f8bf71737d1
Mark Refaat <mark1711712@miuegypt.edu.eg>
2021-07-18 02:56:36 +0200

    Update VideoTile
    Update ancher to VideoTile icon to go to #/video/123
    Update VideoIcon
    Change width to max-width for video-icon

--- src/components/VideoTile.vue
+++ src/components/VideoTile.vue
@@ -1,6 +1,6 @@
 <template>
-    <a href="#" class="video-container">
-        <img class="video-icon" src="http://unsplash.it/36/36?gravity=center" />
+    <a href="#/video/123" class="video-container">
+        <img class="video-icon" src="https://images.unsplash.com/photo-1558637845-c8b7ead71a3e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8MTYlM0E5fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" />
         <div class="video-details">
             <div class="video-title">SpongeBob in Real Life Episode 2</div>
             <div class="video-data">JChaseFilms <br> 54,013,577 views</div>
@@ -27,9 +27,9 @@ export default {
 }
 
 .video-icon {
-  margin-right: 15px;
+  margin-right: 10px;
   background-color: #AAA;
-  width: 300px;
+  max-width: 300px;
   height: 100px;
 }
 
