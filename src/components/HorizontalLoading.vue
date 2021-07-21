c2b8f1b1997f37c4b7c93e1d2ab094b5bc432ebf
Mark Refaat <mark1711712@miuegypt.edu.eg>
2021-07-21 13:24:24 +0200

    Create HorizontalLoading Component

---
+++ src/components/HorizontalLoading.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="progress">
+        <div class="progress-bar"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "HorizontalLoading",
+};
+</script>
+
+<style scoped>
+.progress{
+    display: block;
+    height: 5px;
+    background-color: #E9ECEF;
+    position: relative;
+}
+
+.progress .progress-bar{
+   position: absolute;
+   height: 100%;
+   background-color: red;
+   animation: progress-animation 4s infinite;
+}
+
+
+@keyframes progress-animation{
+    0% { width: 0%; }
+    100% { width: 100%}
+}
+</style>
